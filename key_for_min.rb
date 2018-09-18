d6907f4b488666ec4bbe91795ee789484c349bda
github username <github email address>
2018-09-18 14:16:45 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,51 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+#need to loop over entire hash,
+#then check if value is bigger than PREVIOUS biggest value
+#(like how spaceship op & #sort works under the hood!)
+# advantage here: this shows the 'complexity' of the algorithm to sort values that is 'hidden' by other solutions with 'tricks' like #sort an array, shortcuts like #max/#min...
+
+
+name_hash = {:blake => 500, :zoe => 0, :ashley => 2, :adam => 1}
+
+
+
+    # -- my program: --
+
+def key_for_min_value(hash)
   min_key = nil
   min_value = nil
-  name_hash.each do |key, value|
-    if min_value == nil || value < min_value
+  hash.each do |key, value|
+    if min_value.nil? || value < min_value
       min_key = key
       min_value = value
     end
   end
   min_key
 end
+
+p key_for_min_value(name_hash) # => :zob
+
+
+
+   #--- using #sort and <=> ---
+
+def key_for_min_sort(h)
+  min_key = h.sort { |a, b| b[0] <=> a[0] }
+  min_key.first[0]
+   #first -> gives k:v pair with smallest value. [0] -> first element of pair == key of min value!
+end
+
+p key_for_min_sort(name_hash) # => :zoe
+
+
+
+  #  -- #sort_by ---
+
+def sort_by(hash)
+  hash = hash.keys.sort_by { |k| hash[k] }
+  hash[0]
+end
+
+p sort_by(name_hash) # => :zoe
